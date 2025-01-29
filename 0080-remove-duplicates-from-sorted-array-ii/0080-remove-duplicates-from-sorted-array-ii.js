31471fce22d5c7e4dd4cd12dc83af6ce6eb8b595
TEJU D <111564230+tejudharanesh@users.noreply.github.com>
2025-01-29 22:32:13 +0530

    Time: 68 ms (26.83%), Space: 52.5 MB (25.08%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    if(nums.length < 3){
+        return nums.length;
+    }
+    var i=2;
+    for (let j = 2; j < nums.length; j++) {
+        if (nums[j] !== nums[i - 2]) {
+            nums[i] = nums[j];
+            i++;
+        }
+    }
+    return i;
+};

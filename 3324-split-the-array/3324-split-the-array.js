e02a187d662054d1714076eb6e18dde0bf5c867d
TEJU D <111564230+tejudharanesh@users.noreply.github.com>
2025-03-12 11:07:41 +0530

    Time: 4 ms (25.12%), Space: 55.1 MB (31.63%) - LeetHub

--- 3324-split-the-array/3324-split-the-array.js
+++ 3324-split-the-array/3324-split-the-array.js
@@ -7,12 +7,11 @@ var isPossibleToSplit = function(nums) {
 
     for(let n of nums){
         count.set(n, (count.get(n) || 0) + 1);
-    }
-
-    for(let val of count.values()){
-        if(val > 2){
+        if(count.get(n) > 2){
             return false;
         }
     }
+
+    
     return true;
 };

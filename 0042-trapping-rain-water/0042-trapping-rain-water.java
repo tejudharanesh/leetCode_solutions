ed34132b0b16fafbb70c31dab92c5c673ae90690
TEJU D <111564230+tejudharanesh@users.noreply.github.com>
2025-03-19 11:12:59 +0530

    Time: 1 ms (63.5%), Space: 45.9 MB (88.34%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,25 +1,29 @@
 class Solution {
-    public int trap(int[] h) {
-        int i=0;
-        int j=h.length-1;
-        int leftMax =0;
-        int rightMax=0;
-        int count=0;
+    public int trap(int[] height) {
+        int total = 0;
+        int n = height.length;
+        int[] prefix = new int[n];
+        int[] suffix = new int[n];
 
-        while(i<j){
-            if(h[i]<=h[j]){
-                leftMax = Math.max(h[i],leftMax);
-                count+= leftMax-h[i];
-                i++;
-                
-            }
-            else{
-                rightMax = Math.max(h[j],rightMax);
-                count+= rightMax-h[j];
-                j--;
+        prefix[0] = height[0];
+        suffix[n-1]=height[n-1];
 
+        for(int i=1; i<n;i++){
+            prefix[i] = Math.max(prefix[i-1],height[i]);
+        }
+
+        for(int i=n-2; i>=0;i--){
+            suffix[i] = Math.max(suffix[i+1],height[i]);
+        }
+
+        for(int i=0; i<n;i++){
+            if(height[i] < prefix[i] && height[i] < suffix[i]){
+                total += Math.min(prefix[i],suffix[i]) - height[i];
             }
         }
-        return count;
+
+        return total;
+
+
     }
 }
